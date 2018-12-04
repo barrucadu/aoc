40cc00f50688db096bfdfd91803ebd5ba90fbd8a
Michael Walker <mike@barrucadu.co.uk>
2018-12-04 20:45:34 +0000

    Drop 'unlines' from day 4 parser

--- solutions/lib/Day4Common.hs
+++ solutions/lib/Day4Common.hs
@@ -9,21 +9,21 @@ import Utils
 -- | Parser for day 4, shared between both parts
 parse :: String -> [(Int, [(Int, Int)], Int)]
 {-# INLINABLE parse #-}
-parse = go0 . unlines . sort . lines where
-  go0 ('[':_:_:_:_:'-':_:_:'-':_:_:' ':_:_:':':_:_:']':' ':'G':'u':'a':'r':'d':' ':'#':rest) = go1 0 rest
+parse = go0 . sort . lines where
+  go0 (('[':_:_:_:_:'-':_:_:'-':_:_:' ':_:_:':':_:_:']':' ':'G':'u':'a':'r':'d':' ':'#':rest):rest') = go1 0 rest rest'
   go0 [] = []
   go0 _ = error "malformed input"
 
-  go1 acc (' ':'b':'e':'g':'i':'n':'s':' ':'s':'h':'i':'f':'t':'\n':rest) = go2 acc rest
-  go1 acc (d:rest) = go1 (stepParseInt acc d) rest
-  go1 _ _ = error "malformed input"
+  go1 acc " begins shift" rest' = go2 acc rest'
+  go1 acc (d:rest) rest' = go1 (stepParseInt acc d) rest rest'
+  go1 _ _ _ = error "malformed input"
 
   go2 guardnum xs0 = let (times, count, rest) = fallsAsleep [] 0 xs0 in (guardnum, times, count) : go0 rest where
-    fallsAsleep times !count ('[':_:_:_:_:'-':_:_:'-':_:_:' ':'0':'0':':':m1:m2:']':' ':'f':'a':'l':'l':'s':' ':'a':'s':'l':'e':'e':'p':'\n':rest) =
+    fallsAsleep times !count (('[':_:_:_:_:'-':_:_:'-':_:_:' ':'0':'0':':':m1:m2:"] falls asleep"):rest) =
       wakesUp times count (parseMinute m1 m2) rest
     fallsAsleep times !count rest = (times, count, rest)
 
-    wakesUp times !count sleepTime ('[':_:_:_:_:'-':_:_:'-':_:_:' ':'0':'0':':':m1:m2:']':' ':'w':'a':'k':'e':'s':' ':'u':'p':'\n':rest) =
+    wakesUp times !count sleepTime (('[':_:_:_:_:'-':_:_:'-':_:_:' ':'0':'0':':':m1:m2:"] wakes up"):rest) =
       let wakeTime = parseMinute m1 m2
       in fallsAsleep ((sleepTime, wakeTime) : times) (count + wakeTime - sleepTime) rest
     wakesUp _ _ _ _ = error "malformed input"
