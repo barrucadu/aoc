f05198068b889d08ed379d4f35c1724cd6b2dab6
Michael Walker <mike@barrucadu.co.uk>
2018-12-05 11:55:14 +0000

    Day 5 part 2

---
+++ solutions/day5/Part2.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BangPatterns #-}
+
+import Data.Char (toLower, toUpper)
+import qualified Data.Set as S
+
+main :: IO ()
+main = do
+  input <- init <$> readFile "../inputs/day5.txt"
+  print (solve input)
+
+solve :: String -> Int
+solve input = minimum [ shrink (remove p input) | p <- S.toList polymers ] where
+  polymers = go S.empty input where
+    go acc (p:ps) = go (S.insert (toLower p) acc) ps
+    go acc [] = acc
+
+  remove p = filter (\c -> toLower c /= p)
+
+  shrink = go 0 [] where
+    go !n [] (b:bs) = go (n+1) [b] bs
+    go !n _ [] = n
+    go !n as0@(a:as) (b:bs)
+      | a /= b && toUpper a == toUpper b = go (n-1) as bs
+      | otherwise = go (n+1) (b:as0) bs
