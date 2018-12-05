c913b5bbc666b2c96dad3e93286c0e46b9500afc
Michael Walker <mike@barrucadu.co.uk>
2018-12-05 13:38:36 +0000

    Use an IntSet in day 5 part 2

--- solutions/day5/Part2.hs
+++ solutions/day5/Part2.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 
-import qualified Data.Set as S
+import qualified Data.IntSet as S
+import Data.Char (chr, ord)
 
 import Utils
 
@@ -11,11 +12,11 @@ main = do
 
 solve :: String -> Int
 solve input = minimum [ shrink (remove p input) | p <- S.toList polymers ] where
-  polymers = S.map lowercase (go S.empty input) where
-    go acc (p:ps) = go (S.insert p acc) ps
+  polymers = S.map lowercase' (go S.empty input) where
+    go acc (p:ps) = go (S.insert (ord p) acc) ps
     go acc [] = acc
 
-  remove p = filter (\c -> lowercase c /= p)
+  remove p = filter (\c -> ord (lowercase c) /= p)
 
   shrink = go 0 [] where
     go !n [] (b:bs) = go (n+1) [b] bs
@@ -23,3 +24,5 @@ solve input = minimum [ shrink (remove p input) | p <- S.toList polymers ] where
     go !n as0@(a:as) (b:bs)
       | a /= b && lowercase a == lowercase b = go (n-1) as bs
       | otherwise = go (n+1) (b:as0) bs
+
+  lowercase' = ord . lowercase . chr
