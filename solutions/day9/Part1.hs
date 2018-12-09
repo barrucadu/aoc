9e97e6dd02aa1f71028f44eeaa0a0a332e5bdd5f
Michael Walker <mike@barrucadu.co.uk>
2018-12-09 19:00:56 +0000

    Use an unboxed mutable vector in d9p1

--- solutions/day9/Part1.hs
+++ solutions/day9/Part1.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 
-import Data.IntMap.Strict as M
+import Control.Monad.ST (ST, runST)
+import qualified Data.Vector.Unboxed.Mutable as V
 
 import Utils
 
@@ -18,34 +19,43 @@ parse = go1 0 where
   go2 _ _ = error "invalid input"
 
 solve :: (Int, Int) -> Int
-solve (nplayers, lastmarble) = maximum (M.elems (go scores0 1 0 [] [0])) where
-  go :: M.IntMap Int -> Int -> Int -> [Int] -> [Int] -> M.IntMap Int
-  go scores !marble !player anticlockwise clockwise
-    | marble == lastmarble + 1 = scores
-    | marble `mod` 23 == 0 =
-      let (anticlockwise', (m:clockwise')) = rotateA 7 anticlockwise clockwise
-          (anticlockwise'', clockwise'') = rotateC 1 anticlockwise' clockwise'
-          score = marble + m
-          scores' = M.insertWith (+) player score scores
-          player' = (player + 1) `mod` nplayers
-          marble' = marble + 1
-      in go scores' marble' player' anticlockwise'' clockwise''
-    | otherwise =
-      let (anticlockwise', clockwise') = rotateC 1 anticlockwise clockwise
-          player' = (player + 1) `mod` nplayers
-          marble' = marble + 1
-      in go scores marble' player' anticlockwise' (marble:clockwise')
+solve (nplayers, lastmarble) = runST $ do
+    scores <- V.new nplayers
+    scores' <- go scores 1 0 [] [0]
+    getMax scores'
+  where
+    go :: V.STVector s Int -> Int -> Int -> [Int] -> [Int] -> ST s (V.STVector s Int)
+    go scores = go' where
+      go' !marble !player anticlockwise clockwise
+        | marble == lastmarble + 1 = pure scores
+        | marble `mod` 23 == 0 = do
+          let (anticlockwise', (m:clockwise')) = rotateA 7 anticlockwise clockwise
+          let (anticlockwise'', clockwise'') = rotateC 1 anticlockwise' clockwise'
+          let score = marble + m
+          let player' = (player + 1) `mod` nplayers
+          let marble' = marble + 1
+          V.unsafeModify scores (+score) player
+          go' marble' player' anticlockwise'' clockwise''
+        | otherwise = do
+          let (anticlockwise', clockwise') = rotateC 1 anticlockwise clockwise
+          let player' = (player + 1) `mod` nplayers
+          let marble' = marble + 1
+          go' marble' player' anticlockwise' (marble:clockwise')
 
-  rotateA :: Int -> [Int] -> [Int] -> ([Int], [Int])
-  rotateA _ [] [] = error "rotateA"
-  rotateA 0 as cs = (as, cs)
-  rotateA n as (c:cs) = rotateA (n-1) (c:as) cs
-  rotateA n as [] = rotateA n [] (reverse as)
+    rotateA :: Int -> [Int] -> [Int] -> ([Int], [Int])
+    rotateA _ [] [] = error "rotateA"
+    rotateA 0 as cs = (as, cs)
+    rotateA n as (c:cs) = rotateA (n-1) (c:as) cs
+    rotateA n as [] = rotateA n [] (reverse as)
 
-  rotateC :: Int -> [Int] -> [Int] -> ([Int], [Int])
-  rotateC _ [] [] = error "rotateC"
-  rotateC 0 as cs = (as, cs)
-  rotateC n (a:as) cs = rotateC (n-1) as (a:cs)
-  rotateC n [] cs = rotateC n (reverse cs) []
+    rotateC :: Int -> [Int] -> [Int] -> ([Int], [Int])
+    rotateC _ [] [] = error "rotateC"
+    rotateC 0 as cs = (as, cs)
+    rotateC n (a:as) cs = rotateC (n-1) as (a:cs)
+    rotateC n [] cs = rotateC n (reverse cs) []
 
-  scores0 = M.fromList [(player, 0) | player <- [0..nplayers-1]]
+    getMax v = getMax' (V.length v) 0 where
+      getMax' 0 best = pure best
+      getMax' idx best = do
+        best' <- V.unsafeRead v (idx-1)
+        getMax' (idx-1) (max best best')
