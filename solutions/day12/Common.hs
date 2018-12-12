1e428f5ec7c9b2482159023eeb7274b4305af142
Michael Walker <mike@barrucadu.co.uk>
2018-12-12 22:15:14 +0000

    Day 12: turn transition maps into transition sets

--- solutions/day12/Common.hs
+++ solutions/day12/Common.hs
@@ -1,22 +1,27 @@
 module Common where
 
-import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
-type Transitions = (M.Map String Char, M.Map String Char)
+type Transitions = (S.Set String, S.Set String)
 
 parse :: String -> (String, Transitions)
 {-# INLINABLE parse #-}
 parse = go1 . lines where
-  go1 ((_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:rest):[]:transitions) = (rest, go2 M.empty M.empty transitions)
+  go1 ((_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:rest):[]:transitions) = (rest, go2 S.empty S.empty transitions)
   go1 _ = error "invalid input"
 
-  go2 trdot trhash ([l1, l2, '.', r1, r2, _, _, _, _, d]:rest) =
-    go2 (M.insert [l1, l2, r1, r2] d trdot) trhash rest
-  go2 trdot trhash ([l1, l2, '#', r1, r2, _, _, _, _, d]:rest) =
-    go2 trdot (M.insert [l1, l2, r1, r2] d trhash) rest
-  go2 trdot trhash _ = (trdot, trhash)
+  go2 trdot trhash ([l1, l2, '.', r1, r2, _, _, _, _, '#']:rest) =
+    go2 (S.insert [l1, l2, r1, r2] trdot) trhash rest
+  go2 trdot trhash ([l1, l2, '#', r1, r2, _, _, _, _, '#']:rest) =
+    go2 trdot (S.insert [l1, l2, r1, r2] trhash) rest
+  go2 trdot trhash (_:rest) = go2 trdot trhash rest
+  go2 trdot trhash [] = (trdot, trhash)
 
-findTransition :: (M.Map String Char, M.Map String Char) -> Char -> String -> Char
+findTransition :: Transitions -> Char -> String -> Char
 {-# INLINABLE findTransition #-}
-findTransition (m, _) '.' k = M.findWithDefault '.' k m
-findTransition (_, m)  _  k = M.findWithDefault '.' k m
+findTransition (s, _) '.' k
+  | k `S.member` s = '#'
+  | otherwise      = '.'
+findTransition (_, s)  _  k
+  | k `S.member` s = '#'
+  | otherwise      = '.'
