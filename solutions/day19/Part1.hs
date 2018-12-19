70ab0d93927c88a47b0bd5a72dbf7f08979ae3ea
Michael Walker <mike@barrucadu.co.uk>
2018-12-19 14:15:44 +0000

    Day 19: share evaluator between parts

--- solutions/day19/Part1.hs
+++ solutions/day19/Part1.hs
@@ -1,5 +1,4 @@
 import Control.Monad.ST (runST)
-import qualified Data.Vector.Unboxed as V
 
 import Common
 import Utils
@@ -8,12 +7,7 @@ main :: IO ()
 main = mainFor 19 parse (show . solve)
 
 solve :: (Int, Program) -> Int
-solve (ipR, program) = runST $ go 0 =<< newRegs 0 0 0 0 0 0 where
-  go ip regs = do
-    let instr = V.unsafeIndex program ip
-    setReg regs ipR ip
-    action instr regs
-    ip' <- (+1) <$> getReg regs ipR
-    if ip' >= V.length program
-      then getReg regs 0
-      else go ip' regs
+solve (ipR, program) = runST $ do
+  regs <- newRegs 0 0 0 0 0 0
+  runProgram program ipR regs
+  getReg regs 0
