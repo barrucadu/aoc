fc9ec14df986630ea2312f4e3782a0a7f6a87b2e
Michael Walker <mike@barrucadu.co.uk>
2018-12-20 12:53:39 +0000

    Day 20: use Int instead of default Integer

--- solutions/day20/Common.hs
+++ solutions/day20/Common.hs
@@ -4,9 +4,12 @@ import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
 import qualified Data.Graph.Inductive as G
 
+type Pos = (Int, Int)
+
 parse :: String -> G.UGr
 {-# INLINABLE parse #-}
 parse = go (M.singleton (0,0) 0) [] (0,0) [] where
+  go :: M.Map Pos Int -> [G.Edge] -> Pos -> [Pos] -> String -> G.UGr
   go vs es _ _ [] = G.mkUGraph (M.elems vs) es
   go vs es p ps ('\n':rest) = go vs es p ps rest
   go vs es p ps ('^':rest)  = go vs es p ps rest
