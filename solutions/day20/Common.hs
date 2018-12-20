5dc1cfa0239eb7108945daa4df9cc8336c824af1
Michael Walker <mike@barrucadu.co.uk>
2018-12-20 13:03:55 +0000

    Day 20: use a strict tuple for coordinates

--- solutions/day20/Common.hs
+++ solutions/day20/Common.hs
@@ -4,11 +4,12 @@ import qualified Data.Map.Strict as M
 import Data.Maybe (fromJust)
 import qualified Data.Graph.Inductive as G
 
-type Pos = (Int, Int)
+data Pos = P {-# UNPACK #-} !Int {-# UNPACK #-} !Int
+  deriving (Eq, Ord)
 
 parse :: String -> G.UGr
 {-# INLINABLE parse #-}
-parse = go (M.singleton (0,0) 0) [] (0,0) [] where
+parse = go (M.singleton (P 0 0) 0) [] (P 0 0) [] where
   go :: M.Map Pos Int -> [G.Edge] -> Pos -> [Pos] -> String -> G.UGr
   go vs es _ _ [] = G.mkUGraph (M.elems vs) es
   go vs es p ps ('\n':rest) = go vs es p ps rest
@@ -17,10 +18,10 @@ parse = go (M.singleton (0,0) 0) [] (0,0) [] where
   go vs es p ps ('(':rest)  = go vs es p (p:ps) rest
   go vs es _ (p:ps) (')':rest) = go vs es p ps rest
   go vs es _ ps@(p:_) ('|':rest) = go vs es p ps rest
-  go vs es p@(x,y) ps ('N':rest) = step vs es p (x, y-1) ps rest
-  go vs es p@(x,y) ps ('S':rest) = step vs es p (x, y+1) ps rest
-  go vs es p@(x,y) ps ('E':rest) = step vs es p (x-1, y) ps rest
-  go vs es p@(x,y) ps ('W':rest) = step vs es p (x+1, y) ps rest
+  go vs es p@(P x y) ps ('N':rest) = step vs es p (P x (y-1)) ps rest
+  go vs es p@(P x y) ps ('S':rest) = step vs es p (P x (y+1)) ps rest
+  go vs es p@(P x y) ps ('E':rest) = step vs es p (P (x-1) y) ps rest
+  go vs es p@(P x y) ps ('W':rest) = step vs es p (P (x+1) y) ps rest
   go _ _ _ _ _ = error "invalid input"
 
   step vs es p p' ps rest =
