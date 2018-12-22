f30a31a35108bd34355d71b5df4495278773121e
Michael Walker <mike@barrucadu.co.uk>
2018-12-22 18:21:43 +0000

    Day 22 part 1 (needs benchmark)

--- README.md
+++ README.md
@@ -45,3 +45,4 @@ Current benchmark results:
 |Day19Part2|0.038808531|+-  0.28%|
 |Day20Part1|0.020788557|+-  0.09%|
 |Day20Part2|0.020314709|+-  0.07%|
+|Day22Part1|-|-|
---
+++ solutions/day22/Part1.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE BangPatterns #-}
+
+import Control.Monad.ST (ST, runST)
+import Data.Foldable (for_)
+
+import Utils
+
+main :: IO ()
+main = mainFor 22 parse (show . solve)
+
+parse :: String -> (Int, (Int, Int))
+parse input0 = (depth, target) where
+  (dline:tline:_) = lines input0
+
+  depth  = parseInt (drop (length "depth: ")  dline)
+  target = go 0 (drop (length "target: ") tline)
+
+  go !acc (',':rest) = (acc, parseInt rest)
+  go !acc (c:rest) = go (stepParseInt acc c) rest
+  go _ _ = error "invalid input"
+
+solve :: (Int, (Int, Int)) -> Int
+solve (depth, (tX, tY)) = runST $ do
+    gIs <- newArray (tX+1) (tY+1)
+    eLs <- newArray (tX+1) (tY+1)
+
+    setGIEL gIs eLs 0  0  0
+    for_ [1..tX] $ \x -> setGIEL gIs eLs x 0 (x * 16807)
+    for_ [1..tY] $ \y -> setGIEL gIs eLs 0 y (y * 48271)
+    for_ [1..tX] $ \x ->
+      for_ [1..tY] $ \y -> calcGIEL gIs eLs x y
+    setGIEL gIs eLs tX tY 0
+
+    riskLevel eLs
+  where
+    setGIEL :: STArray s Int -> STArray s Int -> Int -> Int -> Int -> ST s ()
+    setGIEL gIs eLs x y gi = do
+      let el = (gi + depth) `mod` 20183
+      writeArray gIs x y gi
+      writeArray eLs x y el
+
+    calcGIEL :: STArray s Int -> STArray s Int -> Int -> Int -> ST s ()
+    calcGIEL gIs eLs x y = do
+      left <- readArray eLs (x-1) y
+      above <- readArray eLs x (y-1)
+      let gi = left * above
+      setGIEL gIs eLs x y gi
+
+    riskLevel :: STArray s Int -> ST s Int
+    riskLevel eLs = go 0 0 0 where
+      go !acc x y
+        | x > tX = go acc 0 (y+1)
+        | y > tY = pure acc
+        | otherwise = do
+            el <- readArray eLs x y
+            go (acc + (el `mod` 3)) (x+1) y
