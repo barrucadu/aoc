89758a680eb5cccf549bbfc5ffb6bb6f98d6fe60
Michael Walker <mike@barrucadu.co.uk>
2019-12-06 09:54:54 +0000

    2019d5: remove unused pattern constructors

--- 2019/solutions/day05/Common.hs
+++ 2019/solutions/day05/Common.hs
@@ -9,34 +9,34 @@ import qualified Data.Vector.Unboxed.Mutable as VUM
 
 import           Utils
 
-pattern OpAddPP  <- 0001 where OpAddPP  = 0001
-pattern OpAddPI  <- 1001 where OpAddPI  = 1001
-pattern OpAddIP  <- 0101 where OpAddIP  = 0101
-pattern OpAddII  <- 1101 where OpAddII  = 1101
-pattern OpMulPP  <- 0002 where OpMulPP  = 0002
-pattern OpMulPI  <- 1002 where OpMulPI  = 1002
-pattern OpMulIP  <- 0102 where OpMulIP  = 0102
-pattern OpMulII  <- 1102 where OpMulII  = 1102
-pattern OpRead   <- 0003 where OpRead   = 0003
-pattern OpWriteP <- 0004 where OpWriteP = 0004
-pattern OpWriteI <- 0104 where OpWriteI = 0104
-pattern OpJmtPP  <- 0005 where OpJmtPP  = 0005
-pattern OpJmtPI  <- 1005 where OpJmtPI  = 1005
-pattern OpJmtIP  <- 0105 where OpJmtIP  = 0105
-pattern OpJmtII  <- 1105 where OpJmtII  = 1106
-pattern OpJmfPP  <- 0006 where OpJmfPP  = 0005
-pattern OpJmfPI  <- 1006 where OpJmfPI  = 1006
-pattern OpJmfIP  <- 0106 where OpJmfIP  = 0106
-pattern OpJmfII  <- 1106 where OpJmfII  = 1106
-pattern OpLtPP   <- 0007 where OpLtPP   = 0007
-pattern OpLtPI   <- 1007 where OpLtPI   = 1007
-pattern OpLtIP   <- 0107 where OpLtIP   = 0107
-pattern OpLtII   <- 1107 where OpLtII   = 1107
-pattern OpEqPP   <- 0008 where OpEqPP   = 0008
-pattern OpEqPI   <- 1008 where OpEqPI   = 1008
-pattern OpEqIP   <- 0108 where OpEqIP   = 0108
-pattern OpEqII   <- 1108 where OpEqII   = 1108
-pattern OpHlt    <- 0099 where OpHlt    = 0099
+pattern OpAddPP  <- 0001
+pattern OpAddPI  <- 1001
+pattern OpAddIP  <- 0101
+pattern OpAddII  <- 1101
+pattern OpMulPP  <- 0002
+pattern OpMulPI  <- 1002
+pattern OpMulIP  <- 0102
+pattern OpMulII  <- 1102
+pattern OpRead   <- 0003
+pattern OpWriteP <- 0004
+pattern OpWriteI <- 0104
+pattern OpJmtPP  <- 0005
+pattern OpJmtPI  <- 1005
+pattern OpJmtIP  <- 0105
+pattern OpJmtII  <- 1105
+pattern OpJmfPP  <- 0006
+pattern OpJmfPI  <- 1006
+pattern OpJmfIP  <- 0106
+pattern OpJmfII  <- 1106
+pattern OpLtPP   <- 0007
+pattern OpLtPI   <- 1007
+pattern OpLtIP   <- 0107
+pattern OpLtII   <- 1107
+pattern OpEqPP   <- 0008
+pattern OpEqPI   <- 1008
+pattern OpEqIP   <- 0108
+pattern OpEqII   <- 1108
+pattern OpHlt    <- 0099
 
 type Program = [Int]
 type Memory s = VUM.STVector s Int
