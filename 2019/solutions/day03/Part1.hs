21b59575fe1dd8340fe537682e35dd2ba267d21c
Michael Walker <mike@barrucadu.co.uk>
2019-12-06 11:31:22 +0000

    2019d3p1: compute intersection incrementally

--- 2019/solutions/day03/Part1.hs
+++ 2019/solutions/day03/Part1.hs
@@ -8,37 +8,35 @@ import           Utils
 main :: IO ()
 main = mainFor 3 parse (show . solve)
 
-parse :: String -> (S.Set (Int, Int), S.Set (Int, Int))
+parse :: String -> S.Set (Int, Int)
 parse = go . lines where
-  go (wire1:wire2:_) = (goW S.empty 0 0 wire1, goW S.empty 0 0 wire2)
+  go (wire1:wire2:_) =
+    let wire1Path = goW (const True) S.empty 0 0 wire1
+    in goW (`S.member` wire1Path) S.empty 0 0 wire2
   go _ = error "bad input"
 
-  goW !set !x !y ('R':rest) =
+  goW p !set !x !y ('R':rest) =
     let (range, rest') = goI 0 rest
-        set' = S.union set (S.fromList [(x + d, y) | d <- [0..range]])
-    in goW set' (x + range) y rest'
-  goW !set !x !y ('L':rest) =
+        set' = S.union set (S.fromList [point | d <- [0..range], let point = (x + d, y), p point])
+    in goW p set' (x + range) y rest'
+  goW p !set !x !y ('L':rest) =
     let (range, rest') = goI 0 rest
-        set' = S.union set (S.fromList [(x - d, y) | d <- [0..range]])
-    in goW set' (x - range) y rest'
-  goW !set !x !y ('U':rest) =
+        set' = S.union set (S.fromList [point | d <- [0..range], let point = (x - d, y), p point])
+    in goW p set' (x - range) y rest'
+  goW p !set !x !y ('U':rest) =
     let (range, rest') = goI 0 rest
-        set' = S.union set (S.fromList [(x, y - d) | d <- [0..range]])
-    in goW set' x (y - range) rest'
-  goW !set !x !y ('D':rest) =
+        set' = S.union set (S.fromList [point | d <- [0..range], let point = (x, y - d), p point])
+    in goW p set' x (y - range) rest'
+  goW p !set !x !y ('D':rest) =
     let (range, rest') = goI 0 rest
-        set' = S.union set (S.fromList [(x, y + d) | d <- [0..range]])
-    in goW set' x (y + range) rest'
-  goW !set _ _ _ = set
+        set' = S.union set (S.fromList [point | d <- [0..range], let point = (x, y + d), p point])
+    in goW p set' x (y + range) rest'
+  goW _ !set _ _ _ = set
 
   goI !acc (',':rest) = (acc, rest)
   goI !acc (c:rest) = goI (stepParseInt acc c) rest
   goI !acc [] = (acc, [])
 
-solve :: (S.Set (Int, Int), S.Set (Int, Int)) -> Int
-solve (wire1, wire2) = minimum . map (manhattan2 origin)  $ S.toList crossings where
-  crossings = S.intersection wire1' wire2'
-  wire1' = S.delete origin wire1
-  wire2' = S.delete origin wire2
-
+solve :: S.Set (Int, Int) -> Int
+solve = minimum . map (manhattan2 origin) . S.toList . S.delete origin where
   origin = (0, 0)
