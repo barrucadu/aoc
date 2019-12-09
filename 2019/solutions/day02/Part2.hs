a023ad86297ba353ce51badb7de77d0a109fc35d
Michael Walker <mike@barrucadu.co.uk>
2019-12-09 19:36:07 +0000

    2019d2p2: use clone

--- 2019/solutions/day02/Part2.hs
+++ 2019/solutions/day02/Part2.hs
@@ -8,17 +8,20 @@ main :: IO ()
 main = mainFor 2 parse (show . solve)
 
 solve :: Program -> Int
-solve program = runST $ search [(noun, verb) | noun <- [0..99], verb <- [0..99]] where
-  search ((noun, verb):rest) = do
-    mem <- initialise program
-    VUM.unsafeWrite mem 1 noun
-    VUM.unsafeWrite mem 2 verb
-    runNoIO mem
-    res <- VUM.unsafeRead mem 0
-    if res == target
-      then pure (100 * noun + verb)
-      else search rest
-  search [] = error "noun and verb not found"
+solve program = runST $ do
+    mem0 <- initialise program
+    search mem0 [(noun, verb) | noun <- [0..99], verb <- [0..99]]
+  where
+    search mem0 ((noun, verb):rest) = do
+      mem <- VUM.clone mem0
+      VUM.unsafeWrite mem 1 noun
+      VUM.unsafeWrite mem 2 verb
+      runNoIO mem
+      res <- VUM.unsafeRead mem 0
+      if res == target
+        then pure (100 * noun + verb)
+        else search mem0 rest
+    search _ [] = error "noun and verb not found"
 
 target :: Int
 target = 19690720
