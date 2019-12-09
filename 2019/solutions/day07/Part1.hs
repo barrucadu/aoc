3fe00715efffe5d027122bdd32003e5a1b0e73ec
Michael Walker <mike@barrucadu.co.uk>
2019-12-09 13:45:13 +0000

    2019d7: use foldl'

--- 2019/solutions/day07/Part1.hs
+++ 2019/solutions/day07/Part1.hs
@@ -1,4 +1,4 @@
-import           Data.List (permutations)
+import           Data.List (foldl', permutations)
 
 import           Common
 import           Intcode   (Program, parse)
@@ -8,11 +8,8 @@ main :: IO ()
 main = mainFor 7 parse (show . solve)
 
 solve :: Program -> Int
-solve program = go 0 (permutations [0,1,2,3,4]) where
-  go best [] = best
-  go best (phase:phases) =
-    let candidate = go' phase
-    in go (max candidate best) phases
+solve program = go (permutations [0,1,2,3,4]) where
+  go = foldl' (\best phase -> max (go' phase) best) 0
 
   go' [pA,pB,pC,pD,pE] = runNetwork program pA pB pC pD pE
   go' _ = error "invalid phase specification"
