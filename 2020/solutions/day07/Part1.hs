926096fe9a32dc3db0e2e3060e54526ef84b5d9f
Michael Walker <mike@barrucadu.co.uk>
2020-12-07 11:24:59 +0000

    2020d7

---
+++ 2020/solutions/day07/Part1.hs
@@ -0,0 +1,49 @@
+import Data.List (foldl')
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+import Utils
+
+main :: IO ()
+main = mainFor 7 parse (show . solve)
+
+solve :: InvertedBagRules -> Int
+solve rules = go seen0 target0 where
+  go :: S.Set Colour -> S.Set Colour -> Int
+  go seen candidates
+    | S.null candidates = S.size seen - 1
+    | otherwise =
+      let col = S.elemAt 0 candidates
+          seen' = S.insert col seen
+          candidates' = S.delete col candidates `S.union` (grab col `S.difference` seen)
+      in go seen' candidates'
+
+  seen0 = S.singleton target
+  target0 = grab target
+
+  grab k = M.findWithDefault S.empty k rules
+
+target :: Colour
+target = ("shiny", "gold")
+
+-------------------------------------------------------------------------------
+
+type Colour = (String, String)
+
+type InvertedBagRules = M.Map Colour (S.Set Colour)
+
+parse :: String -> InvertedBagRules
+parse = foldl' parseLine M.empty . map words . lines where
+  parseLine :: InvertedBagRules -> [String] -> InvertedBagRules
+  parseLine acc (c1:c2:"bags":"contain":contains) = parseContains (c1, c2) acc contains
+  parseLine _ _ = error "invalid input"
+
+  parseContains :: Colour -> InvertedBagRules -> [String] -> InvertedBagRules
+  parseContains col = parseContains' where
+    parseContains' :: InvertedBagRules -> [String] -> InvertedBagRules
+    parseContains' acc ["no", "other", "bags."] = acc
+    parseContains' acc (_:c1:c2:_:contains) =
+      let acc' = M.insertWith S.union (c1, c2) (S.singleton col) acc
+      in parseContains' acc' contains
+    parseContains' acc [] = acc
+    parseContains' _ _ = error "invalid input"
---
+++ 2020/solutions/day07/Part2.hs
@@ -0,0 +1,41 @@
+import Data.List (foldl')
+import qualified Data.Map as M
+
+import Utils
+
+main :: IO ()
+main = mainFor 7 parse (show . solve)
+
+solve :: BagRules -> Int
+solve rules = go1 target where
+  go1 = go2 . M.toList . grab
+
+  go2 ((col, count):cs) = count * go1 col + count + go2 cs
+  go2 [] = 0
+
+  grab k = M.findWithDefault M.empty k rules
+
+target :: Colour
+target = ("shiny", "gold")
+
+-------------------------------------------------------------------------------
+
+type Colour = (String, String)
+
+type BagRules = M.Map Colour (M.Map Colour Int)
+
+parse :: String -> BagRules
+parse = foldl' parseLine M.empty . map words . lines where
+  parseLine :: BagRules -> [String] -> BagRules
+  parseLine acc (c1:c2:"bags":"contain":contains) = parseContains (c1, c2) acc contains
+  parseLine _ _ = error "invalid input"
+
+  parseContains :: Colour -> BagRules -> [String] -> BagRules
+  parseContains col = parseContains' where
+    parseContains' :: BagRules -> [String] -> BagRules
+    parseContains' acc ["no", "other", "bags."] = acc
+    parseContains' acc (n:c1:c2:_:contains) =
+      let acc' = M.insertWith M.union col (M.singleton (c1, c2) (parseInt n)) acc
+      in parseContains' acc' contains
+    parseContains' acc [] = acc
+    parseContains' _ _ = error "invalid input"
